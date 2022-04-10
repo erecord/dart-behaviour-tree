f2861fbcdda906153ddd4bb8546ed9a11f8fbea0
Elliott Record <elliott.record@gmail.com>
2022-04-10 14:42:07 +0100

    Updated main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,17 @@
+import 'failing_action_node.dart';
+import 'node_iterator.dart';
+import 'print_message_action_node.dart';
+import 'sequence_node.dart';
+
 void main() {
-  print("Hello world!");
+  final node1 = PrintMessageActionNode("message 1 ");
+  final node2 = PrintMessageActionNode("message 2 ");
+  final node3 = FailingActionNode();
+
+  final nodeIterator = NodeIterator([node1, node3, node2]);
+  final compositeNode = SequenceNode(nodeIterator);
+
+  final result = compositeNode.evaluate();
+
+  print("Tree Result: $result");
 }
